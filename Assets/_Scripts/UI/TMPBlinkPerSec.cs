8763e6e6bcd50260caaa3178a36e3994be253f6b
Anastasia <av@anastasia.io>
2018-04-20 13:35:14 -0700

    updated screen, refined article display, added more screen animation

---
+++ Assets/_Scripts/UI/TMPBlinkPerSec.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class TMPBlinkPerSec : MonoBehaviour {
+
+    private TextMeshProUGUI textToBlink;
+
+
+    void Start()
+    {
+        textToBlink = this.GetComponent<TextMeshProUGUI>();
+        StartCoroutine(Blink(textToBlink));
+    }
+
+
+    //a coroutine that blinks every second
+    private IEnumerator Blink(TextMeshProUGUI text)
+    {
+        while (true)
+        {
+            text.enabled = true;
+            yield return new WaitForSeconds(1f);
+            text.enabled = false;
+            yield return new WaitForSeconds(1f);
+        }
+
+    }
+}
---
+++ Assets/_Scripts/UI/TMPDateTime.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class TMPDateTime : MonoBehaviour {
+
+    private TextMeshProUGUI timeText;
+    public bool year, month, day, time, dayString, monthString, dateString;
+
+    // Use this for initialization
+    void Start () {
+        timeText = this.GetComponent<TextMeshProUGUI>();
+        DateTime dateTime = DateTime.Now;
+
+        if (day)
+        {
+            timeText.text = dateTime.ToString("dd");
+        }
+        if (month)
+        {
+            timeText.text = dateTime.ToString("MM");
+        }
+        if (year)
+        {
+            timeText.text = dateTime.ToString("yyyy");
+        }
+        if (dayString)
+        {
+            timeText.text = dateTime.ToString("dddd");
+        }
+        if (monthString)
+        {
+            timeText.text = dateTime.ToString("MMMM");
+        }
+        if (dateString)
+        {
+            timeText.text = (dateTime.ToString("dddd") + " " + dateTime.ToString("dd") + " " + dateTime.ToString("MMMM"));
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        DateTime dateTime = DateTime.Now;
+
+        if (time)
+        {
+
+            timeText.text = dateTime.ToString("h:mm" + " " + "tt");
+        }
+
+
+
+    }
+}
